34be189d95c12064b321f60398606bd1260de0fa
PlutoA713N <mallachandu99@gmail.com>
2024-05-23 19:59:59 +0530

    Add App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignIn from './components/SignIn';
+
+const App: React.FC = () => {
+  return (
+    <div >
+      <SignIn />
+    </div>
+  );
+};
+
+export default App;
