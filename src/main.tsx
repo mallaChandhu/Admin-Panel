01fbca1bbe4e1822acd5b0f6784d5887e1336c74
mallaChandhu <chandu.malla@galvix.com>
2024-05-25 00:57:11 +0530

    Add Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Routes>
+    <Route index element={<Navigate to='/login' />} />
+    <Route  path='/login' element={ <App />} />
+  </Routes>
+  </BrowserRouter>
 )
